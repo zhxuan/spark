38986891089d2b8f77c962c522203e69a3082cfe
zhangx <11464510@qq.com>
2015-07-24 09:09:43 +0800

    Test Sucess

--- examples/src/main/scala/org/apache/spark/examples/SparkPi.scala
+++ examples/src/main/scala/org/apache/spark/examples/SparkPi.scala
@@ -24,7 +24,7 @@ import org.apache.spark._
 /** Computes an approximation to pi */
 object SparkPi {
   def main(args: Array[String]) {
-    val conf = new SparkConf().setAppName("Spark Pi").setMaster("local[2]")
+    val conf = new SparkConf().setAppName("Spark Pi")
     val spark = new SparkContext(conf)
     val slices = if (args.length > 0) args(0).toInt else 2
     val n = math.min(100000L * slices, Int.MaxValue).toInt // avoid overflow
